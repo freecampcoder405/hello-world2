b385e57375bb57d1231ecb88571eb8b7922a7e41
harshithansraj <hansrajharshit@gmail.com>
2023-04-30 23:50:44 +0530

    Create q1creation.sql
    DDL for q1

---
+++ DBS Labs/Lab 8/q1creation.sql
@@ -0,0 +1,5 @@
+create table student_table(rollno number(6),gpa numeric(2,1));
+ insert into student_table values(1,5.8);
+ insert into student_table values(2,6.5);
+ insert into student_table values(3,3.4);
+ insert into student_table values(4,7.8);
